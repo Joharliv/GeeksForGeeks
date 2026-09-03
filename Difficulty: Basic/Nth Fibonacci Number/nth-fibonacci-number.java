5856aeb135a949f2e95d9cca239e3e0af0897b9c
Liv Johar <liv2545.beai24@chitkara.edu.in>
2026-09-03 14:39:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Nth Fibonacci Number/nth-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    static int nthFibonacci(int n) {
+        // code here
+        if(n <= 1)return n;
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for(int i = 2;i<= n;i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
